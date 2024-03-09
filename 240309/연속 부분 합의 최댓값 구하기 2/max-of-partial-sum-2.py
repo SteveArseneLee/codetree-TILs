fd98b0bf100f67ce585216bfaae9308613c0282c
SquidEngineer <lclgood@khu.ac.kr>
2024-03-09 17:24:42 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 138ms, Memory: 32MB, Status: Passed - CodeTree

--- 240309/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
+++ 240309/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -1,13 +1,31 @@
 import sys
-si = sys.stdin.readline
-n = int(input())
-arr = list(map(int, si().split()))
 
-val = arr[0]
-for i in range(1, n):
-    tmp = arr[i]
-    if val < 0:
-        val = max(tmp, val+tmp)
+INT_MIN = -sys.maxsize
+
+# 변수 선언 및 입력
+n = int(input())
+arr = [0] + list(map(int, input().split()))
+
+# 최댓값을 구해야 하는 문제이므로
+# 초기값을 INT_MIN으로 설정합니다.
+ans = INT_MIN
+
+# 현재 연속 부분 수열 내 원소의 합을
+# 저장합니다.
+sum_of_nums = 0;
+
+for i in range(1, n + 1):
+    # 만약 현재 연속 부분 수열 내 원소의 합이
+    # 0보다 작아진다면, 지금부터 새로운
+    # 연속 부분 수열을 만드는 것이 더 유리합니다.
+    if sum_of_nums < 0:
+        sum_of_nums = arr[i]
+    
+    # 그렇지 않다면 기존 연속 부분 수열에 
+    # 현재 원소를 추가하는 것이 더 좋습니다.
     else:
-        val += tmp
-print(val)
+        sum_of_nums += arr[i]
+    
+    ans = max(ans, sum_of_nums)
+
+print(ans)
