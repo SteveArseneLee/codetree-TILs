f97e01158fc63d3acf33f3707005ab08bd96f391
SquidEngineer <lclgood@khu.ac.kr>
2024-03-09 12:13:49 +0900

    Title: 최고의 33위치, Time: 126ms, Memory: 28MB, Status: Passed - CodeTree

--- 240309/최고의 33위치/best-place-of-33.py
+++ 240309/최고의 33위치/best-place-of-33.py
@@ -1,21 +1,34 @@
+# 변수 선언 및 입력
 n = int(input())
-grid = [list(map(int, input().split())) for _ in range(n)]
-# print(grid)
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
 
-def check_gold(x,y):
+
+# (row_s, col_s) ~ (row_e, col_e) 사이의 금의 개수를 계산합니다.
+def get_num_of_gold(row_s, col_s, row_e, col_e):
     num_of_gold = 0
-    for row in range(x, x+3):
-        for col in range(y, y+3):
+
+    for row in range(row_s, row_e + 1):
+        for col in range(col_s, col_e + 1):
             num_of_gold += grid[row][col]
+
     return num_of_gold
 
+
 max_gold = 0
-for i in range(n):
-    for j in range(n):
-        # 좌측 모서리를 기준으로 3*3개
-        if i+2 >= n or j+2 >= n: continue
-        else:
-            gold_amount = check_gold(i,j)
-            # print(gold_amount)
-            max_gold = max(max_gold, gold_amount)
+
+# (row, col)이 3 * 3 격자의 좌측 상단 모서리인 경우를 전부 탐색합니다. 
+for row in range(n):
+    for col in range(n):
+        # 3 * 3 격자가 n * n 격자를 벗어나는 경우는 계산하지 않습니다.
+        if row + 2 >= n or col + 2 >= n:
+            continue
+        
+        num_of_gold = get_num_of_gold(row, col, row + 2, col + 2)
+            
+        # 최대 금의 개수를 저장합니다.
+        max_gold = max(max_gold, num_of_gold)
+
 print(max_gold)
