3516841fe153c3b80ba3fbff6858477efa76f762
SquidEngineer <lclgood@khu.ac.kr>
2024-03-09 17:38:42 +0900

    Title: 오름 내림차순 정렬, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240309/오름 내림차순 정렬/inc-dec-sorting.py
+++ 240309/오름 내림차순 정렬/inc-dec-sorting.py
@@ -3,8 +3,8 @@ n = int(input())
 arr = list(map(int, input().split()))
 arr.sort()
 for i in arr:
-    sys.stdout.write(i + ' ')
+    sys.stdout.write(str(i) + ' ')
 sys.stdout.write('\n')
 arr.reverse()
 for i in arr:
-    sys.stdout.write(i + ' ')
+    sys.stdout.write(str(i) + ' ')
