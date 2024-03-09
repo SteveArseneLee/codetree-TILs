574b7f3d62815948118482ff12c7d84c0fe8a3ff
SquidEngineer <lclgood@khu.ac.kr>
2024-03-09 18:15:23 +0900

    Title: 중앙값 계산 2, Time: 235ms, Memory: 24MB, Status: Passed - CodeTree

--- 240309/중앙값 계산 2/get-median-2.py
+++ 240309/중앙값 계산 2/get-median-2.py
@@ -1,8 +1,11 @@
 n = int(input())
 arr = list(map(int, input().split()))
-for i in range(n):
+ans = []
+for i in range(1,n+1):
     # 홀수면
-    if (arr[i]%2) != 0:
-        # i번째까지 출력
-        # print(arr[:i+1])
-        print(arr[(i+1)//2])
+    if i%2 != 0:
+        tmp = arr[:i]
+        tmp.sort()
+        ans.append(tmp[i//2])
+for i in ans:
+    print(i, end=' ')
