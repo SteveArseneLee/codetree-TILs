f6375e6936635a4a7c6a66832239ecb7ddcefe58
SquidEngineer <lclgood@khu.ac.kr>
2024-03-09 14:08:39 +0900

    Title: 특정 수와 근접한 합, Time: 110ms, Memory: 25MB, Status: Passed - CodeTree

--- 240309/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240309/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -1,10 +1,26 @@
 import sys
-from itertools import combinations
-n, s = map(int, input().split())
+
+INT_MAX = sys.maxsize
+
+# 변수 선언 및 입력:
+n, s = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
 
-min_value = sys.maxsize
-# 2개 제외한 합을 구하기
-for i in combinations(arr, n-2):
-    min_value = min(min_value, abs(sum(i)-s))
-print(min_value)
+array_sum = 0
+ans = INT_MAX
+
+# 배열의 값들의 총합을 미리 구해둡니다.
+for elem in arr:
+    array_sum += elem
+
+# 모든 쌍을 다 잡아봅니다.
+for i in range(n):
+    for j in range(i + 1, n):
+        # i번과 j번 수를 제외할 경우 남은 숫자들의 총합은 다음과 같습니다.
+        new_sum = array_sum - arr[i] - arr[j]
+
+        diff = abs(new_sum - s)
+        ans = min(ans, diff)
+
+# 정답을 출력합니다.
+print(ans)
