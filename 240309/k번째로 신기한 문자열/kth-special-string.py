dfcd9c956215a0e71d6faca21a853ea62e91f8f2
SquidEngineer <lclgood@khu.ac.kr>
2024-03-09 17:58:33 +0900

    Title: k번째로 신기한 문자열, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240309/k번째로 신기한 문자열/kth-special-string.py
+++ 240309/k번째로 신기한 문자열/kth-special-string.py
@@ -1,6 +1,8 @@
 n,k,t = list(input().split())
+len_t = len(t)
+# print(len_t)
 arr = [input() for i in range(int(n))]
-ans = [i for i in arr if t in i]
+ans = [i for i in arr if t in i[:len_t]]
 # print(ans)
 ans.sort()
 # print(ans)
