bcf6c3bf76ed742210cdace130fe5949d2191765
SquidEngineer <lclgood@khu.ac.kr>
2024-03-09 16:06:15 +0900

    Title: 숨은 단어 찾기 2, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240309/숨은 단어 찾기 2/find-hidden-words-2.py
+++ 240309/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -1,38 +1,40 @@
-n,m = map(int, input().split())
-arr = [list(input()) for _ in range(n)]
-# print(arr)
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
+arr = [
+    input()
+    for _ in range(n)
+]
 
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < m
 
-# 'L'이 있다면 8가지 방향으로 확인
-def check(i,j):
-    ans = 0
-    if i-2 >= 0 and arr[i-1][j] == arr[i-2][j] == 'E':
-        ans += 1
-    if i+2 < n and arr[i+1][j] == arr[i+2][j] == 'E':
-        ans += 1
+dxs, dys = [1, 1, 1, -1, -1, -1, 0, 0], [-1, 0, 1, -1, 0, 1, -1, 1]
 
-    if j-2 >= 0 and arr[i][j-1] == arr[i][j-2] == 'E':
-        ans += 1
-    if j+2 < n and arr[i][j+1] == arr[i][j+2] == 'E':
-        ans += 1
-
-    if i-2 >= 0 and j-2 >= 0 and arr[i-1][j-1] == arr[i-2][j-2] == 'E':
-        ans += 1
-    if i+2 < n and j+2 < n and arr[i+1][j+1] == arr[i+2][j+2] == 'E':
-        ans += 1
-
-    if i-2 >= 0 and j+2 < n and arr[i-1][j+1] == arr[i-2][j+2] == 'E':
-        ans += 1
-    if i+2 < n and j-2 >= 0 and arr[i+1][j-1] == arr[i+2][j-2] == 'E':
-        ans += 1
-    return ans
-
-# print(arr)
-result = 0
+# 모든 좌표에서 다 확인해봅니다.
+cnt = 0
 for i in range(n):
-    for j in range(m):
-        # print(arr[i][0])
-        if arr[i][j] == 'L':
-            result += check(i,j)
+	# 격자를 벗어나지 않을 범위로만 잡습니다.
+	for j in range(m):
+		
+		if arr[i][j] != 'L':
+			continue
+		
+		for dx, dy in zip(dxs, dys):
+			curt = 1
+			curx = i
+			cury = j
+			while True:
+				nx = curx + dx
+				ny = cury + dy
+				if in_range(nx, ny) == False:
+					break
+				if arr[nx][ny] != 'E':
+					break
+				curt += 1
+				curx = nx
+				cury = ny
+			
+			if curt >= 3:
+				cnt += 1
 
-print(result)
+print(cnt)
