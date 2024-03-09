bd09a8d8c74d690ca8140785ff82194acd7d5077
SquidEngineer <lclgood@khu.ac.kr>
2024-03-09 14:23:38 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 240309/괄호 쌍 만들어주기 3/pair-parentheses-3.py
+++ 240309/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -1,16 +1,29 @@
-test = list(input())
-len_test = len(test)
-# '('가 있는 위치에서 항상 테스트
-def check(i):
-    result = 0
-    for x in range(i, len_test):
-        if test[x] == ')':
-            result += 1
-    return result
+# test = list(input())
+# len_test = len(test)
+# # '('가 있는 위치에서 항상 테스트
+# def check(i):
+#     result = 0
+#     for x in range(i, len_test):
+#         if test[x] == ')':
+#             result += 1
+#     return result
 
+# cnt = 0
+# for i in range(len_test):
+# # for i in test:
+#     if test[i] == '(':
+#         cnt += check(i)
+# print(cnt)
+
+# 변수 선언 및 입력
+string = input()
+n = len(string)
+
+# 모든 쌍을 다 잡아봅니다.
 cnt = 0
-for i in range(len_test):
-# for i in test:
-    if test[i] == '(':
-        cnt += check(i)
+for i in range(n):
+    for j in range(i + 1, n):
+        if string[i] == '(' and string[j] == ')':
+            cnt += 1
+            
 print(cnt)
