cc3cda596209a68992d62802f2801b6a09eb9a55
SquidEngineer <lclgood@khu.ac.kr>
2024-03-09 15:41:23 +0900

    Title: 이상한 진수 2, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240309/이상한 진수 2/awkward-digits-2.py
+++ 240309/이상한 진수 2/awkward-digits-2.py
@@ -1,8 +1,11 @@
 a = list(input())
-for i in range(len(a)):
-    if a[i] == '0':
-        a[i] = '1'
-        break
+if '0' not in a:
+    a[-1] = '0'
+else:
+    for i in range(len(a)):
+        if a[i] == '0':
+            a[i] = '1'
+            break
 
 x = ''.join(a)
 # print(type(x))
