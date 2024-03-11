b208bdd7ab486844efeee1952de4042954d96851
SquidEngineer <lclgood@khu.ac.kr>
2024-03-12 00:57:41 +0900

    Title: G or H 3, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240312/G or H 3/G-or-H-3.py
+++ 240312/G or H 3/G-or-H-3.py
@@ -1,3 +1,5 @@
+# G와 H중 받은 알파벳의 팻말을 들고 있음
+# 사진의 크기는 k
 MAX_NUM = 10000
 n, k = map(int, input().split())
 pics = [list(input().split()) for _ in range(n)]
@@ -7,14 +9,15 @@ pics = sorted(pics, key=lambda x: int(x[0]))
 max_value = int(pics[-1][0])
 min_value = int(pics[0][0])
 road = [0] * (max_value+1)
-
+print(min_value, max_value)
 for i,j in pics:
     if j == 'G':
         road[int(i)] = 1
     elif j == 'H':
         road[int(i)] = 2
-# print(road)
+print(road)
 ans = 0
-for i in range(min_value, MAX_NUM + 1 - k):
+for i in range(MAX_NUM + 1 - k):
+    # print("t: ", road[i:i+k+1])
     ans = max(ans, sum(road[i:i+k+1]))
 print(ans)
