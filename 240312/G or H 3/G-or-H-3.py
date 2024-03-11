7fa3c6d5639ec9f2d486220bdd5720d1d2ea1187
SquidEngineer <lclgood@khu.ac.kr>
2024-03-12 00:50:16 +0900

    Title: G or H 3, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240312/G or H 3/G-or-H-3.py
+++ 240312/G or H 3/G-or-H-3.py
@@ -3,9 +3,11 @@
 n, k = map(int, input().split())
 pics = [list(input().split()) for _ in range(n)]
 
-# pics = sorted(pics, key=lambda x: int(x[0]))
-max_value = int(max(pics, key=lambda x: int(x[0]))[0])+1
-road = [0] * max_value
+pics = sorted(pics, key=lambda x: int(x[0]))
+# max_value = int(max(pics, key=lambda x: int(x[0]))[0])+1
+max_value = int(pics[-1][0])
+min_value = int(pics[0][0])
+road = [0] * (max_value+1)
 
 for i,j in pics:
     if j == 'G':
@@ -14,6 +16,6 @@ for i,j in pics:
         road[int(i)] = 2
 
 ans = 0
-for i in range(max_value + 1 - k):
+for i in range(min_value, max_value + 1 - k):
     ans = max(ans, sum(road[i:i+k+1]))
 print(ans)
