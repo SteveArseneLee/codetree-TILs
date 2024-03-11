cc921c2b74333e54cc3bec7605ddcea696711bad
SquidEngineer <lclgood@khu.ac.kr>
2024-03-12 00:57:58 +0900

    Title: G or H 3, Time: 221ms, Memory: 29MB, Status: Passed - CodeTree

--- 240312/G or H 3/G-or-H-3.py
+++ 240312/G or H 3/G-or-H-3.py
@@ -9,13 +9,13 @@ pics = sorted(pics, key=lambda x: int(x[0]))
 max_value = int(pics[-1][0])
 min_value = int(pics[0][0])
 road = [0] * (max_value+1)
-print(min_value, max_value)
+# print(min_value, max_value)
 for i,j in pics:
     if j == 'G':
         road[int(i)] = 1
     elif j == 'H':
         road[int(i)] = 2
-print(road)
+# print(road)
 ans = 0
 for i in range(MAX_NUM + 1 - k):
     # print("t: ", road[i:i+k+1])
