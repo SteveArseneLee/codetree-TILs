2630c561bf85c79946bbacc8945012cf8c14afb8
SquidEngineer <lclgood@khu.ac.kr>
2024-03-12 22:29:22 +0900

    Title: 가장 짧은 부분합, Time: 150ms, Memory: 32MB, Status: Passed - CodeTree

--- 240312/가장 짧은 부분합/shortest-subtotal.py
+++ 240312/가장 짧은 부분합/shortest-subtotal.py
@@ -1,43 +1,18 @@
 import sys
 
-INT_MAX = sys.maxsize
+si = sys.stdin.readline
+n, S = list(map(int, si().split()))
+a = list(map(int, si().split()))
+R, sum, ans = -1, 0, n + 1
+for L in range(n):
+    while R + 1 < n and sum < S:
+        R += 1
+        sum += a[R]
 
-# 변수 선언 및 입력:
-n, s = tuple(map(int, input().split()))
-arr = [0] + list(map(int, input().split()))
+    if sum >= S:
+        ans = min(ans, R - L + 1)
 
+    sum -= a[L]
 
-# 가능한 구간 중 가장 짧은 길이를 구합니다.
-ans = INT_MAX
-
-# 구간을 잡아봅니다.
-sum_val = 0
-j = 0
-for i in range(1, n + 1):
-    # 구간 내 합이 s보다 작으면 계속 진행합니다.
-    while j + 1 <= n and sum_val < s:
-        sum_val += arr[j + 1]
-        j += 1
-
-    # 만약 최대한 이동했는데도
-    # sum_val이 s가 되지 못했다면
-    # 탐색을 종료합니다.
-    if sum_val < s:
-        break
-    
-    # 현재 구간 [i, j]는 
-    # i를 시작점으로 하는
-    # 가장 짧은 구간이므로
-    # 구간 크기 중 최솟값을 갱신합니다.
-    ans = min(ans, j - i + 1)
-
-    # 다음 구간으로 넘어가기 전에
-    # arr[i]에 해당하는 값은 구간에서 제외시킵니다.
-    sum_val -= arr[i]
-
-# 만약 불가능하다면
-# -1을 답으로 합니다.
-if ans == INT_MAX:
-    ans = -1
-
+if ans == n + 1: ans = -1
 print(ans)
