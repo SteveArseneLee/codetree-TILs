5b79fff9f9a657a8d4aa9a6c20d36a5ab315c19b
SquidEngineer <lclgood@khu.ac.kr>
2024-03-14 23:39:55 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 174ms, Memory: 26MB, Status: Passed - CodeTree

--- 240314/한 가지로 열리는 자물쇠/one-way-lock.py
+++ 240314/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -1,9 +1,16 @@
+# 변수 선언 및 입력
 n = int(input())
-arr = list(map(int, input().split()))
-# arr[i]과의 차가 2 이하인 애들만 골라봐
-ans = 0
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        for k in range(1,n+1):
-            if abs(arr[0] - i ) <= 2 or abs(arr[1] - j) <= 2 or abs(arr[2]-k)<=2: ans+=1
-print(ans)
+a, b, c = tuple(map(int, input().split()))
+
+
+# 모든 조합을 다 만들어 봅니다.
+cnt = 0
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        for k in range(1, n + 1):
+            # 한 자리라도 주어진 조합과의 거리가 2 이내인지 확인합니다.
+            if abs(a - i) <= 2 or abs(b - j) <= 2 or \
+               abs(c - k) <= 2:
+                cnt += 1
+
+print(cnt)
