76472c0b9e9e68a834b2c1b27a0cc1f80e0f78ae
SquidEngineer <lclgood@khu.ac.kr>
2024-03-14 23:07:19 +0900

    Title: 되돌아오기 2, Time: 124ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240314/되돌아오기 2/come-back-2.py
@@ -0,0 +1,23 @@
+test_case = list(input())
+x,y = 0,0
+# 북, 동, 남, 서
+dx, dy = (0,1,0,-1),(1,0,-1,0)
+dir = 0
+elapsed_time = 0
+for i in test_case:
+    if i == 'L':
+        dir = (dir+1)%4
+        elapsed_time += 1
+    elif i == 'R':
+        dir = (dir+3)%4
+        elapsed_time += 1
+    # F면 이동
+    else:
+        x,y = x+dx[dir], y+dy[dir]
+        elapsed_time += 1
+        # print(x,y)
+
+    if x==0 and y==0:
+        print(elapsed_time)
+        break
+if elapsed_time == 0: print(-1)
