84e7b66bc80d5105745803c6fafb3f5466201e87
SquidEngineer <lclgood@khu.ac.kr>
2024-03-16 21:13:59 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 183ms, Memory: 25MB, Status: Passed - CodeTree

--- 240316/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
+++ 240316/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
@@ -6,14 +6,14 @@ answer = [0 for _ in range(n)]
 
 # Custom Comparator를 활용한 정렬
 numbers.sort(key=lambda x: (x[0], x[1]))
-print(numbers)
+# print(numbers)
 
 # 정렬된 숫자들의 원래 인덱스를 활용한 정답 배열 저장
 # 새로 저장된 인덱스, 값, 원래 인덱스
 for i, (j, index) in enumerate(numbers):
-    print("i: ",i, "j: ", j, "index: ", index)
+    # print("i: ",i, "j: ", j, "index: ", index)
     answer[index] = i + 1 # 인덱스 보정
-    print(answer)
+    # print(answer)
 
 # 출력
 for i in range(n):
