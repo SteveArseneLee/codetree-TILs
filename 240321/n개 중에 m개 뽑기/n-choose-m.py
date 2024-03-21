0acd1ee868d8d84c40c7181b879d0a1adb038c29
SquidEngineer <lclgood@khu.ac.kr>
2024-03-21 23:53:38 +0900

    Title: n개 중에 m개 뽑기, Time: 146ms, Memory: 25MB, Status: Passed - CodeTree

--- 240321/n개 중에 m개 뽑기/n-choose-m.py
+++ 240321/n개 중에 m개 뽑기/n-choose-m.py
@@ -1,27 +1,18 @@
 import sys
 n,m = map(int, input().split())
-selected=[0] * m
+selected=[]
 used=[False]*(n+1)
 ans = []
-def rec_func(k):
-    if k == m:
-        ans.append(selected[:])
-    else:
-        for cand in range(1,n+1):
-            if not used[cand]:
-                selected[k] = cand
-                used[cand] = True
-                rec_func(k+1)
-                selected[k] = 0
-                used[cand] = False
+def rec_func(start_num, k):
+    if start_num == n+1:
+        if k == m:
+            for i in selected:
+                print(i, end=' ')
+            print()
+        return
+    selected.append(start_num)
+    rec_func(start_num+1, k+1)
+    selected.pop()
+    rec_func(start_num+1, k)
 
-rec_func(0)
-real_ans = []
-for i in ans:
-    i.sort()
-    if i not in real_ans:
-        real_ans.append(i)
-for i in real_ans:
-    for j in i:
-        print(j,end=' ')
-    print()
+rec_func(1,0)
