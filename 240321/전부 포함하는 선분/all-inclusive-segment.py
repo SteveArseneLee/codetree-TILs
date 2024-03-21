b8c3e38a3da1e133ee839eff11ad3677f89d78f7
SquidEngineer <lclgood@khu.ac.kr>
2024-03-21 17:28:34 +0900

    Title: 전부 포함하는 선분, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240321/전부 포함하는 선분/all-inclusive-segment.py
+++ 240321/전부 포함하는 선분/all-inclusive-segment.py
@@ -1,11 +1,18 @@
 import sys
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-ans = sys.maxsize
+arr = sorted([list(map(int, input().split())) for _ in range(n)])
+# 결과를 저장할 변수
+min_length = float('inf')
+
 for i in range(n):
-    tmp = arr[:i] + arr[i+1:]
-    # print(tmp)
-    tmp = sorted(tmp, key=lambda x: (x[0], x[1]))
-    # print(tmp)
-    ans = min(ans, tmp[-1][-1] - tmp[0][0])
-print(ans)
+    # i번째 선분을 제외한 나머지 선분들의 최소 시작점과 최대 종료점 찾기
+    min_start = min(arr[j][0] for j in range(n) if j != i)
+    max_end = max(arr[j][1] for j in range(n) if j != i)
+
+    # i번째 선분을 제외했을 때의 선분 길이 계산
+    length = max_end - min_start
+
+    # 최소 길이 업데이트
+    min_length = min(min_length, length)
+
+print(min_length)
