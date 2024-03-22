0cece53825e026b3f40e01331d1c4c2c2144b196
SquidEngineer <lclgood@khu.ac.kr>
2024-03-22 14:59:11 +0900

    Title: 숫자 등장 횟수, Time: 208ms, Memory: 34MB, Status: Passed - CodeTree

--- 240322/숫자 등장 횟수/number-frequency.py
+++ 240322/숫자 등장 횟수/number-frequency.py
@@ -1,6 +1,15 @@
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
-nums = list(map(int, input().split()))
 
-for i in nums:
-    print(arr.count(i), end=' ')
+freq = dict()
+
+for elem in arr:
+    if elem not in freq: freq[elem] =1
+    else: freq[elem] += 1
+
+queries = list(map(int, input().split()))
+for num in queries:
+    if num not in freq:
+        print(0, end=' ')
+    else:
+        print(freq[num], end=' ')
