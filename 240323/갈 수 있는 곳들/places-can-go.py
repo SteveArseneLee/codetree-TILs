eb47bdb24cfc9557baa726ce6cec5ab5b52c9f8f
SquidEngineer <lclgood@khu.ac.kr>
2024-03-23 07:00:24 +0900

    Title: 갈 수 있는 곳들, Time: 226ms, Memory: 27MB, Status: Passed - CodeTree

--- 240323/갈 수 있는 곳들/places-can-go.py
+++ 240323/갈 수 있는 곳들/places-can-go.py
@@ -1,40 +1,34 @@
 from collections import deque
-n,k = map(int, input().split())
+n, k = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
-visited=[[False]*n for _ in range(n)]
-cnt = 0
-
-def can_go(x,y):
-    return 0<=x<n and 0<=y<n and grid[x][y] == 0
-
-dir = [(1,0),(-1,0),(0,1),(0,-1)]
+visited = [[False for _ in range(n)] for _ in range(n)]
 
 q = deque()
+def in_range(x,y): return 0 <=x<n and 0<=y<n
+def can_go(x,y): return in_range(x,y) and not visited[x][y] and not grid[x][y]
+
 def bfs():
-    global cnt
     while q:
         x,y = q.popleft()
-        cnt += 1
-        for dx, dy in dir:
-            nx,ny= x+dx,y+dy
-            if can_go(nx, ny) and not visited[nx][ny]:
-                q.append((nx,ny))
-                visited[nx][ny] = True
+        dxs, dys = [0,0,-1,1],[1,-1,0,0]
 
+        for dx, dy in zip(dxs, dys):
+            new_x, new_y = x + dx, y+dy
+            if can_go(new_x,new_y):
+                q.append((new_x, new_y))
+                visited[new_x][new_y] = True
 
 for _ in range(k):
-    x,y = map(int, input().split())
-    nx, ny= x-1, y-1
-    q.append((nx,ny))
-    visited[nx][ny] = True
+    x, y= tuple(map(int, input().split()))
+    q.append((x-1, y-1))
+    visited[x-1][y-1] = True
+
 bfs()
-    # cnt += 1
-    # print(cnt)
-print(cnt)
-# ans = sum([
-#     1
-#     for i in range(n)
-#     for j in range(n)
-#     if visited[i][j]
-# ])
-# print(ans)
+
+ans = sum([
+    1
+    for i in range(n)
+    for j in range(n)
+    if visited[i][j]
+])
+print(ans)
