7bb20701b445eb44b48849cd224746ba5813a67d
SquidEngineer <lclgood@khu.ac.kr>
2024-04-08 00:11:52 +0900

    Title: 문자 받아 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240408/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       SCanner sc = new Scanner(System.in);
+       String s = sc.next();
+       char c = s.charAt(0);
+       System.out.println(c);
+    }
+}
