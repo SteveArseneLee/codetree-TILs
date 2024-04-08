4916df729a8d9417b0293d7cd1933e376d86bafb
SquidEngineer <lclgood@khu.ac.kr>
2024-04-09 08:02:06 +0900

    Title: hashmap 기본, Time: 488ms, Memory: 21MB, Status: Passed - CodeTree

--- 240409/hashmap 기본/hashmap-basic.java
+++ 240409/hashmap 기본/hashmap-basic.java
@@ -1,35 +1,41 @@
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.util.HashMap;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
-public class Main {    
-    // 변수 선언
-    public static int n;
-    public static HashMap<Integer, Integer> m = new HashMap<>();
+public class Main{
+    public static void main(String[] args) throws IOException{
+        HashMap<Integer, Integer> m = new HashMap<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 입력:
-        n = sc.nextInt();
-        
-        for(int i = 0; i < n; i++) {
-            String command = sc.next();
+        int n = Integer.parseInt(st.nextToken());
+        for (int i = 0; i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
 
-            if(command.equals("add")) {
-                int k = sc.nextInt();
-                int v = sc.nextInt();
-                m.put(k, v);
-            }
-            else if(command.equals("remove")) {
-                int k = sc.nextInt();
+            if (command.equals("add")){
+                int k = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
+                m.put(k,v);
+            } else if (command.equals("remove")){
+                int k = Integer.parseInt(st.nextToken());
                 m.remove(k);
-            }
-            else {
-                int k = sc.nextInt();
-                if(!m.containsKey(k))
-                    System.out.println("None");
+            } else{
+                int k = Integer.parseInt(st.nextToken());
+                if (!m.containsKey(k))
+                    bw.write("None" + "\n");
                 else
-                    System.out.println(m.get(k));
+                    bw.write(m.get(k) + "\n");
             }
+
         }
+        bw.flush();
+        br.close();
+        bw.close();
     }
 }
