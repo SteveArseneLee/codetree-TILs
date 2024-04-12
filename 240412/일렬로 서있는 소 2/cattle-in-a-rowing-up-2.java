2e6113dcc3d9100055564bd11c13e56e1a94fc12
SquidEngineer <lclgood@khu.ac.kr>
2024-04-12 11:22:29 +0900

    Title: 일렬로 서있는 소 2, Time: 110ms, Memory: 9MB, Status: Passed - CodeTree

--- 240412/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
+++ 240412/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -16,7 +16,7 @@ public class Main{
         for (int i = 0; i<n;i++){
             for(int j = i+1; j<n;j++){
                 for(int k = j+1; k<n;k++){
-                    if ((arr[i] < arr[j]) & (arr[j] < arr[k])){
+                    if ((arr[i] <= arr[j]) & (arr[j] <= arr[k])){
                         ans += 1;
                     }
                 }
