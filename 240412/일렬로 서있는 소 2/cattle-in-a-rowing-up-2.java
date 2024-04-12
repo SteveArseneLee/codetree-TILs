c0eefe1f456c9b0e58ac514e4e6d0c9a170e3024
SquidEngineer <lclgood@khu.ac.kr>
2024-04-12 11:21:38 +0900

    Title: 일렬로 서있는 소 2, Time: 100ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240412/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,27 @@
+import java.io.*; import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i<n;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int ans = 0;
+        for (int i = 0; i<n;i++){
+            for(int j = i+1; j<n;j++){
+                for(int k = j+1; k<n;k++){
+                    if ((arr[i] < arr[j]) & (arr[j] < arr[k])){
+                        ans += 1;
+                    }
+                }
+            }
+        }
+        System.out.print(ans);
+    }
+}
